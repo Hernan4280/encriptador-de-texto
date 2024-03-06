8e41ee035e63c22c14532d5086527be522b35b6e
Hernán Bautista <hernann.80@gmail.com>
2024-03-06 18:49:41 -0300

    Correccion de la validacion de texto

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ function borrar() {
 
 function validarTexto(texto) {
     if (/[^a-z\s]/gm.test(texto)) {
-        alert('Solo se permite letras minusculas');
+        alert('Solo se permite letras minusculas sin acentos.');
         window.location.reload();
     } else if (texto.length == 0 || !(/(?:.*[a-zA-Z0-9]){1}/gm.test(texto))) {
-        alert('Campo de texto vacío');
+        alert('Campo de texto vacío.');
         window.location.reload();
     }
 }
