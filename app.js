3f829c6633a86c17f3bfb836719088a51ee0fcbe
Hernán Bautista <hernann.80@gmail.com>
2024-03-05 22:55:25 -0300

    Creo el boton copiar y le doy funcionalidad

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 document.getElementById("miTextarea").addEventListener("click", borrar);
-// myFunction() borra el texto predeterminado al hacer clic en el textarea.
+// borrar() borra el texto predeterminado al hacer clic en el textarea.
 function borrar() {
     document.getElementById("button1").disabled = false;
     document.getElementById("miTextarea").innerHTML = "";
+}
+
+function validarTexto(texto) {
+    if (texto.length == 0 || !(/(?:.*[a-z]){1}/gm.test(texto))) {
+        alert('Campo de texto vacío');
+        window.location.reload();
+    }
+    if (/[^a-z\s]/gm.test(texto)) {
+        alert('Solo se permite letras minusculas');
+        window.location.reload();
+    }
+}
+
+function botonCopiar(txtEncriptado) {
+    const nuevoBoton = document.createElement("button");
+    nuevoBoton.innerHTML = "Copiar";
+    nuevoBoton.setAttribute("id", "mi-btn");
+
+    const miBoton = document.getElementById("btn-copiar");
+    miBoton.appendChild(nuevoBoton);
+
+    nuevoBoton.onclick = async function copiarAlPortapapeles() {
+        try {
+            await navigator.clipboard.writeText(txtEncriptado);
+            console.log('Contenido copiado al portapapeles', txtEncriptado);
+            document.getElementById("miTextarea").value = "";
+            /* Resuelto - texto copiado al portapapeles con éxito */
+          } catch (err) {
+            console.error('Error al copiar: ', err);
+            /* Rechazado - fallo al copiar el texto al portapapeles */
+          }
+    }
 }
 
 const llaves =  {
@@ -13,13 +45,25 @@ const llaves =  {
     u: "ufat"
 }
 
-function encriptar() {
+function encriptar(texto) {
+    const copiaTxt = texto.split('');
+    for (let k in llaves) {
+        for (let i=0; i < texto.length; i++) {
+            if (k === texto[i]) {
+                copiaTxt[i] = llaves[k];
+            }
+        }
+    }
+    return copiaTxt;
+}
+
+
+let bloqueo = false;
+function btnEncriptar() {
 
     const texto = document.getElementById("miTextarea").value;
-
-    if (/[^a-z\s]/gm.test(texto)) {
-        alert('Solo se permite letras minusculas');
-    }
+    
+    validarTexto(texto);
 
     let img = document.getElementById('imagen');
     img.style.display = 'none';
@@ -29,14 +73,14 @@ function encriptar() {
     p1.innerHTML = "";
     p2.innerHTML = "";
 
-    const encriptada = texto.split('');
-    for (let k in llaves) {
-        for (let i=0; i < texto.length; i++) {
-            if (k === texto[i]) {
-                encriptada[i] = llaves[k];
-            }
-        }
-    }
     let salidaEncriptada = document.querySelector('.output-text');
-    salidaEncriptada.innerHTML = encriptada.join('');
+    salidaEncriptada.innerHTML = encriptar(texto).join('');
+
+    const txtEncriptado = document.querySelector('.output-text').innerHTML;
+    if (bloqueo === false) {
+        botonCopiar(txtEncriptado);
+        bloqueo = true;
+    }
+
+    document.getElementById("button1").disabled = true;
 }
